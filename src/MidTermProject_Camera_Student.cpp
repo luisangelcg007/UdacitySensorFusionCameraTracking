8545e63593547b2666798fbe344c156672787288
luisangelcg007 <luisangelcg007@gmail.com>
2021-07-06 14:37:19 -0500

    adding element

--- src/MidTermProject_Camera_Student.cpp
+++ src/MidTermProject_Camera_Student.cpp
@@ -82,6 +82,7 @@ int main(int argc, const char *argv[])
     const std::vector<std::string> selectorTypes = { "SEL_KNN" };
 
     std::vector<TimeInformation> timeInformation;
+    TimeInformation auxiliaryTimeInformation;
 
     for (int detectorTypeIndex = 0; detectorTypeIndex < detectorTypes.size(); detectorTypeIndex++ )
     {
@@ -92,11 +93,17 @@ int main(int argc, const char *argv[])
                 for (int selectorTypeIndex = 0; selectorTypeIndex < matcherTypes.size(); selectorTypeIndex++ ) 
                 {
                     if (!isValidDescriptorDetectorCombo(descriptorTypes[descriptorTypeIndex], detectorTypes[detectorTypeIndex])) { continue; }
-                    timeInformation.push_back(
+                    auxiliaryTimeInformation.detectorType = detectorTypes[detectorTypeIndex];
+                    auxiliaryTimeInformation.descriptorType = descriptorTypes[descriptorTypeIndex];
+                    auxiliaryTimeInformation.matcherType = matcherTypes[matcherTypeIndex];
+                    auxiliaryTimeInformation.selectorType = selectorTypes[matcherTypeIndex];
+                    /*timeInformation.push_back(
                         TimeInformation(detectorTypes[detectorTypeIndex], 
                                         descriptorTypes[descriptorTypeIndex], 
                                         matcherTypes[matcherTypeIndex], 
-                                        selectorTypes[matcherTypeIndex]));
+                                        selectorTypes[matcherTypeIndex]));*/
+                    timeInformation.push_back(
+                        TimeInformation(auxiliaryTimeInformation);
                 }
             }
         }
